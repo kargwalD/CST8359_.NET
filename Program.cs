cd7869d4647e8ef65af8a0ab06cd80bcc077bcb1
kargwalD <kargwaldhruv@gmail.com>
2023-05-19 19:02:59 -0400

    Program.cs- now 1. has been completed using IList and StreamReader

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@
     {
         static void Main(string[] args)
         {
+
+            IList<string> words = new List<string>();
+
+            string fileName="Words.txt";
+
             Console.WriteLine("Choose an option:");
             Console.WriteLine("1 - Import Words from File\r\n2 - Bubble Sort words\r\n3 - LINQ/Lambda sort words\r\n4 - Count the Distinct Words\r\n5 - Take the last 50 words\r\n6 - Reverse print the words\r\n7 - Get and display words that end with 'd' and display the count\r\n8 - Get and display words that start with ‘r’ and display the count\r\n9 - Get and display words that are more than 3 characters long and start with the letter 'a', and display the count\r\nx – Exit\r\n");
 
@@ -12,13 +17,30 @@
             {
                 case "x": break;
                 case "1": ImportWordsFromFile();
+                    Console.WriteLine("Word Count: " + words.Count);
                     break;
             }
 
             void ImportWordsFromFile()
             {
-                string text = File.ReadAllText("Words.txt");
-                Console.WriteLine(text[0]);
+                try
+                {
+
+                    using (StreamReader sr = new StreamReader(fileName))
+                    {
+                        string line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            words.Add(line);
+                        }
+
+                    }
+
+                }catch (Exception ex) {
+
+                    Console.WriteLine(ex.Message);
+                }
+                //Console.WriteLine("Word Count: " + words.Count);
             }
         }
     }
