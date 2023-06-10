e0da9ab2e2ba08cb98a48de6b0a468b6eede91f8
kargwalD <kargwaldhruv@gmail.com>
2023-06-09 23:35:22 -0400

    Website is running; value of no of monkeys is available in Sing after getting it from the user in SongForm

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    
+        public IActionResult SongForm() => View();
+
+        [HttpPost]
+        public IActionResult Sing()
+        {
+            //ViewBag.noOfMonkeys = Request.Form["noOfMonkeys"];
+            HttpContext.Session.SetString("noOfMonkeys", Request.Form["noOfMonkeys"]);
+
+            return View();
+        }
+
+        public IActionResult CreateStudent() => View();
+
+       /* [HttpPost]
+        public IActionResult DisplayStudent(Student student)
+        {
+            // you will complete this
+        }*/
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
